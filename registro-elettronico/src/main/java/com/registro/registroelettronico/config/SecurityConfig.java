6a7968ed6accb56863f012071dce14e1c62c38a8
Ejran <35053200+Tiracsius@users.noreply.github.com>
2025-08-25 17:54:02 +0200

    Implementazione CorsConfig x il forntend

--- registro-elettronico/src/main/java/com/registro/registroelettronico/config/SecurityConfig.java
+++ registro-elettronico/src/main/java/com/registro/registroelettronico/config/SecurityConfig.java
@@ -19,6 +19,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -32,13 +34,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/h2-console/**") // Disabilita CSRF solo per H2
+                        .ignoringRequestMatchers("/h2-console/**")
                         .disable()
                 )
-                .headers(AbstractHttpConfigurer::disable // Permette a H2 di usare i frame
+                .cors(withDefaults()) // Attiva il CORS configurato in CorsConfig
+                .headers(headers -> headers
+                        .frameOptions().disable() // Consente l'uso dei frame per H2 console
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/h2-console/**").permitAll() // Accesso libero a auth e H2
+                        .requestMatchers("/api/auth/**", "/h2-console/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(sess -> sess
