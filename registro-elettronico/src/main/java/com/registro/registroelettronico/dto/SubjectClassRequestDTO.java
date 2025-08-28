4992bb2490b27157891cf4bb00d9e71d33739886
John Benedict <johnbenedictyriarte0303@gmail.com>
2025-08-28 15:58:59 +0200

    small changes for SubjectClassRequestDTO

--- registro-elettronico/src/main/java/com/registro/registroelettronico/dto/SubjectClassRequestDTO.java
+++ registro-elettronico/src/main/java/com/registro/registroelettronico/dto/SubjectClassRequestDTO.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 @Data
@@ -18,6 +19,6 @@ public class SubjectClassRequestDTO {
     private UUID materiaId;
 
     @NotNull
-    private UUID classeId;
+    private List<UUID> classeIds;
 
 }
