428699953139e34ef95f8ba481b17d25395c76b5
John Benedict <johnbenedictyriarte0303@gmail.com>
2025-09-02 14:52:53 +0200

    feature: assign Teacher to SubjectClass, get All SubjectClass by ClassId

--- registro-elettronico/src/main/java/com/registro/registroelettronico/service/SubjectClassService.java
+++ registro-elettronico/src/main/java/com/registro/registroelettronico/service/SubjectClassService.java
@@ -8,4 +8,7 @@ import com.registro.registroelettronico.dto.SubjectClassResponseDTO;
 public interface SubjectClassService {
 	
 	public List<SubjectClassResponseDTO> getAllSubjectClassByTeacherId(UUID teacherId);
+	public List<SubjectClassResponseDTO> getAllSubjectClassByClassId(UUID classId);
+	public void updateSubjectClassById(UUID id, UUID teacherId);
+
 }
