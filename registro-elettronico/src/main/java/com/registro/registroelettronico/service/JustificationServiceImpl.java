86d11d02177c81b88d40f38923e0fd8d4c72242d
John Benedict <johnbenedictyriarte0303@gmail.com>
2025-09-04 15:23:47 +0200

    feature: logic to change PresenceRecord to EXCUSED when the Justification is created

--- registro-elettronico/src/main/java/com/registro/registroelettronico/service/JustificationServiceImpl.java
+++ registro-elettronico/src/main/java/com/registro/registroelettronico/service/JustificationServiceImpl.java
@@ -4,6 +4,7 @@ import com.registro.registroelettronico.dto.JustificationRequestDTO;
 import com.registro.registroelettronico.dto.JustificationResponseDTO;
 import com.registro.registroelettronico.entity.Justification;
 import com.registro.registroelettronico.entity.PresenceRecord;
+import com.registro.registroelettronico.enums.PresenceStatus;
 import com.registro.registroelettronico.exception.JustificationNotFoundException;
 import com.registro.registroelettronico.exception.PresenceRecordNotFoundException;
 import com.registro.registroelettronico.mapper.JustificationMapper;
@@ -11,6 +12,7 @@ import com.registro.registroelettronico.repository.JustificationRepository;
 import com.registro.registroelettronico.repository.PresenceRecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
@@ -22,7 +24,7 @@ public class JustificationServiceImpl implements JustificationService{
     private final PresenceRecordRepository presenceRecordRepository;
     private final JustificationMapper justificationMapper;
 
-
+    @Transactional
     @Override
     public void createJustification(JustificationRequestDTO request) {
         PresenceRecord presenceRecord = presenceRecordRepository.findById(request.getPresenceRecordId())
@@ -30,6 +32,8 @@ public class JustificationServiceImpl implements JustificationService{
 
         Justification justification = justificationMapper.toEntity(request, presenceRecord);
         justificationRepository.save(justification);
+        presenceRecord.setStatus(PresenceStatus.EXCUSED);
+        presenceRecordRepository.save(presenceRecord);
     }
 
     @Override
